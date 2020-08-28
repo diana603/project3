8c745cd3756b7e5101414fbae706e1b2805c5cac
diana603 <stingray2091@gmail.com>
2020-08-27 22:25:08 -0400

    added to beneficiary form linked the bene button from customer page to the actual form

--- client/src/components/FormButton.js
+++ client/src/components/FormButton.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import customer from '../pages/customer.css';
 import {Button} from 'react-bootstrap';
 import Modal from "./Modal";
+import {Link} from "react-router-dom";
 
 
 const FormButton = props => {
@@ -14,15 +15,17 @@ const FormButton = props => {
     <div>
          <h2>Please Select From The Following Forms </h2>
         <div className="formbtn">
+            <Link to="/BeneficiaryForm">
             <Button onClick={handleShow} variant="primary" size="lg">
                Change of Beneficiary
             </Button>
+            </Link>
             <Button  onClick={handleShow}  variant="primary" size="lg">
                   Change of Ownership 
             </Button> 
             <Button  onClick={handleShow} variant="primary" size="lg">
                   W9 Form 
-            </Button>
+            </Button> 
         </div>
         <Modal show={show} handleHide={handleHide}/>
     </div> 
--- client/src/pages/BeneficiaryForm.js
+++ client/src/pages/BeneficiaryForm.js
@@ -64,9 +64,20 @@ const BeneficiaryForm = () => {
 
         <div>
             <p>
-                Beneficiary Designations 
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                                                                                                                                                            
+Subject to the terms of the policy(ies) and any assignee on record with John Hancock, the undersigned hereby revokes all beneficiary
+designations or directions of payment previously made in respect to the proceeds payable on the death of the life insured under the above
+policy(ies) and directs that such proceeds be paid to the beneficiary(ies) listed below. To retain any existing beneficiaries currently
+on fi le they must be restated below. For example, if you are changing only the secondary beneficiary, you must restate the primary
+beneficiary(ies). Please note:
+• The insured on the policy cannot be listed as a beneficiary.
+• Designations given in dollar amounts or fractions will not be accepted.
+• If percentages are not provided below, beneficiaries in the same class (primary/secondary) will share equally in any death benefit
+payable to them. If the beneficiaries are unable to be shared equally (e.g., 1/3) we will designate the extra rounded percentile to the
+fi rst listed beneficiary in each class (e.g., 33.34%, 33.33%, 33.33%). We will not accept designations to more than two decimal
+places (e.g., 33.333% will not be accepted).
+To name any of the primary or secondary beneficiary(ies) as irrevocable beneficiaries, write “irrevocable” aft er their name(s).                                                                                                                                                           
             </p>
+        </div>
 
             <Form.Row>
                 <Form.Group as={Col} controlId="formGridEmail">
@@ -167,9 +178,146 @@ const BeneficiaryForm = () => {
         </Form.Group>
         </Form.Row>
 
-
+        <div>
+            <p>
+            Secondary Beneficiary(ies). Percentage of proceeds in this designation must equal 100%.                                        
+            </p>
         </div>
-        {/* to create paragraph use div then indent with p tag */}
+
+        <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>(1)Secondary Beneficiary First Name </Form.Label>
+                <Form.Control type="Policy Number " placeholder="Enter First Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Secondary Beneficiary Last Name </Form.Label>
+                <Form.Control type="Name" placeholder="Enter Last Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Percentage of Proceeds </Form.Label>
+                <Form.Control type="Name" placeholder="Enter Percentages" />
+                </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+                </Form.Group>
+
+        <Form.Group controlId="formGridAddress2">
+         <Form.Label>Address 2</Form.Label>
+         <Form.Control placeholder="Apartment, studio, or floor" />
+         </Form.Group>
+
+        <Form.Row>
+         <Form.Group as={Col} controlId="formGridCity">
+         <Form.Label>City</Form.Label>
+         <Form.Control />
+        </Form.Group>
+
+         <Form.Group as={Col} controlId="formGridState">
+         <Form.Label>State</Form.Label>
+         <Form.Control as="select" defaultValue="Choose...">
+         <option>Select State</option>         
+            {states && states.map((state)=> (
+                <option>{state}</option>
+            ))}
+         <option>...</option>
+         </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+         <Form.Label>Zip</Form.Label>
+         <Form.Control />
+        </Form.Group>
+        </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>(2)Secondary Beneficiary First Name </Form.Label>
+                <Form.Control type="Policy Number " placeholder="Enter First Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Secondary Beneficiary Last Name </Form.Label>
+                <Form.Control type="Name" placeholder="Enter Last Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Percentage of Proceeds </Form.Label>
+                <Form.Control type="Name" placeholder="Enter Percentages" />
+                </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+                </Form.Group>
+
+        <Form.Group controlId="formGridAddress2">
+         <Form.Label>Address 2</Form.Label>
+         <Form.Control placeholder="Apartment, studio, or floor" />
+         </Form.Group>
+
+        <Form.Row>
+         <Form.Group as={Col} controlId="formGridCity">
+         <Form.Label>City</Form.Label>
+         <Form.Control />
+        </Form.Group>
+
+         <Form.Group as={Col} controlId="formGridState">
+         <Form.Label>State</Form.Label>
+         <Form.Control as="select" defaultValue="Choose...">
+         <option>Select State</option>         
+            {states && states.map((state)=> (
+                <option>{state}</option>
+            ))}
+         <option>...</option>
+         </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+         <Form.Label>Zip</Form.Label>
+         <Form.Control />
+        </Form.Group>
+        </Form.Row>
+
+        <div>
+            <p>
+            For residents of Arizona, Arkansas, California, Idaho, Louisiana, Nevada, New Mexico, Puerto Rico, Tennessee, Texas,
+Washington and Wisconsin:
+If you (the owner) live in one of the community property states listed above, and you have named someone other than your spouse as your
+beneficiary, your spouse may have rights to the death benefit of this policy under state law, even if you choose not to name them as your
+beneficiary. Please have your spouse sign below to waive his or her rights to any community property interest in the death benefit.
+If you are unsure of whether these laws apply to you, consult with your legal or tax professional to determine whether a spousal signature is
+required on this form. John Hancock disclaims any responsibility for determining the applicability of community property laws or the validity of
+the requested beneficiary change.
+Note: Use of the term “spouse” on this form refers to the person to whom the owner is legally married, or the policy owner’s domestic partner
+or equivalent as recognized and allowed by federal law, or by state law in your state of residence.
+Spousal Consent:
+I, (print full legal name) , am aware that the owner, named above, has named someone
+other than me to be the beneficiary of this life insurance policy and do hereby consent to the beneficiary designation(s) indicated on this form
+and waive any rights that I may have to the death benefit proceeds of such policy under applicable community property laws. 
+            </p>
+        </div>
+        <Form.Group controlId="formGridAddress1">
+                <Form.Label>Signature Of Owner and Date (mmddyyyy) </Form.Label>
+                <Form.Control placeholder="Sign Here" />
+                </Form.Group>
+        
+        <Form.Group controlId="formGridAddress1">
+                <Form.Label>Signature of Additional Owner or Irrevocable Beneficiary (if applicable) and Date(mmddyyyy) </Form.Label>
+                <Form.Control placeholder="Sign Here" />
+                </Form.Group>  
+ 
+
+        
+
+        
+
+    
+
         <div className="beneBtn">
             <Button className="gobackBtn" variant="primary" type="submit">
                Go Back
