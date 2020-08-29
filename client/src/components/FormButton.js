5f2305ab1f8e2700ff8f16b564dadda87c5c1a5b
Shannon Trainor <sltrainor@outlook.com>
2020-08-29 12:03:02 -0400

    modal hover, button link, and content add

--- client/src/components/FormButton.js
+++ client/src/components/FormButton.js
@@ -5,28 +5,48 @@ import Modal from "./Modal";
 import {Link} from "react-router-dom";
 
 const FormButton = props => {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(false);
+    const [modalInfo, setModalInfo] = useState("");
+    const [modalTitle, setModalTitle] = useState("");
+    const [nextButton, setNextButton] = useState("");
 
-    const handleShow = () => setShow(true)
-    const handleHide = () => setShow(false)
+    const handleShow = () => setShow(true);
+    const handleHide = () => setShow(false);
+    const showBeneficiaryForm = () => {
+        setModalInfo("Here you can complete a Beneficiary Form. Press next to continue.")
+        setModalTitle("Beneficiary Form")
+        setNextButton("Next")
+        setShow(true)
+    };
+    const showChangeForm = () => {
+        setModalTitle("Change of Ownership")
+        setModalInfo("Here you can complete a form for Change of Ownership.")
+        setShow(true)
+    };
+    const showWForm = () => {
+        setModalTitle("W9 Form")
+        setModalInfo("Here you can complete a W9 Form.")
+        setShow(true)
+    };
+
 
     return (
     <div>
          <h2>Please Select From The Following Forms </h2>
         <div className="formbtn">
             <Link to="/BeneficiaryForm">
-            <Button onClick={handleShow} variant="primary" size="lg">
+            <Button onMouseEnter={showBeneficiaryForm} variant="primary" size="lg">
                Change of Beneficiary
             </Button>
             </Link>
-            <Button  onClick={handleShow}  variant="primary" size="lg">
+            <Button  onMouseEnter={showChangeForm}  variant="primary" size="lg">
                   Change of Ownership 
             </Button> 
-            <Button  onClick={handleShow} variant="primary" size="lg" className="WForm">
+            <Button  onMouseEnter={showWForm} variant="primary" size="lg" className="WForm">
                   W9 Form 
             </Button> 
         </div>
-        <Modal show={show} handleHide={handleHide}/>
+        <Modal show={show} handleHide={handleHide} modalTitle={modalTitle} modalInfo={modalInfo} nextButton={nextButton}/>
     </div> 
     )
 }
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import {Button, Modal} from "react-bootstrap";
 import {render} from 'react-dom';
 import modal from "../pages/assets/modal.css";
+import {Link} from "react-router-dom";
 
 
  function ModalView(props) {
     
-    const { show, handleHide } = props;
+    const { show, handleHide, modalInfo, modalTitle, nextButton } = props;
 
-  
     return (
       <>
         <Modal
@@ -18,12 +18,20 @@ import modal from "../pages/assets/modal.css";
           aria-labelledby="example-modal-sizes-title-lg"
         >
           <Modal.Header closeButton>
-            <Modal.Title>
-              Benef Form
-            </Modal.Title>
+            <Modal.Title>{modalTitle}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Info on B.</Modal.Body>
+          <Modal.Body>{modalInfo}</Modal.Body>
+          <Link to="/BeneficiaryForm">
+            <Button>{nextButton}</Button>
+          </Link>
+          {/* <Link to="/BeneficiaryForm">
+            <Button onClick={show} variant="primary" size="lg">
+               Change of Beneficiary
+            </Button>
+            </Link> */}
         </Modal>
+
+
       </>
     );
   }
