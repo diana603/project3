f2b42c399d7fad06d6659a57a5eee7fce8fea41a
sehouse <sehouse@umich.edu>
2020-08-29 13:05:44 -0400

    New backend stuff instead of the old mess.

--- models/user.js
+++ models/user.js
@@ -1,25 +1,46 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new Schema({
-  first_name: {
-    type: String
-  },
-  last_name: {
-    type: String
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    // The email cannot be null, and must be a proper email before creation
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  // Creating a custom method for our User model. This will check if an unhashed
+  // password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
 
-module.exports = User = mongoose.model('users', UserSchema)
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", (user) => {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+  return User;
+};
