d1aa20e794a8141052017f297a03d07f38524137
Lala <luangu2020@gmail.com>
2020-08-29 14:13:12 -0400

    Footer links working

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,10 +7,54 @@ export class Footer extends Component {
     return (
       <div>
         <footer className='footer mt-auto py-3 bg-dark text-white'>
-          <div className='container'><h3>Copyright &copy; 2020 - East Coast Insurance</h3>
-            <h5>Made with love by Angela, Diana, Lynn, Scott and Shannon</h5></div>
-        </footer>
-      </div>
+          {/* <div className='container'><h3>Copyright &copy; 2020 - East Coast Insurance</h3>
+            <h5>Made with love by Angela, Diana, Lynn, Scott and Shannon</h5>
+            <footer className="footer mt-auto py-3 bg-dark text-white"> */}
+          <div className="container">
+            <h3>Copyright &copy; 2020 - East Coast Insurance</h3>
+            <h5>Made with ❤️️ by
+              <a
+                class="hover footer-links"
+                href="https://github.com/lalaggv2"
+                target="_blank"
+              >
+                {' '}Angela
+              </a>
+              ,
+              <a
+                class="hover footer-links"
+                href="https://github.com/diana603"
+                target="_blank"
+              >
+                {' '}Diana
+              </a>
+              ,
+              <a
+                class="hover footer-links"
+                href="https://github.com/LynnBoudreau"
+                target="_blank"
+              >
+                {' '}Lynn
+              </a>
+              ,
+              <a
+                class="hover footer-links"
+                href="https://github.com/sehouse"
+                target="_blank"
+              >
+                {' '}Scott
+              </a>,
+              <a
+                class="hover footer-links"
+                href="https://github.com/shannontrainor"
+                target="_blank"
+              >
+                {' '}Shannon
+              </a>
+            </h5>
+          </div>
+        </footer >
+      </div >
     )
   }
 };
