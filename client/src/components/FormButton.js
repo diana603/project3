f9ce9f943edb38bc1cea14328c09d46a0f718a8c
Shannon Trainor <sltrainor@outlook.com>
2020-09-01 20:58:05 -0400

    added classnames for forms for styling

--- client/src/components/FormButton.js
+++ client/src/components/FormButton.js
@@ -17,31 +17,34 @@ const FormButton = props => {
         setModalInfo("Here you can complete a Beneficiary Form. Press next to continue.")
         setModalTitle("Beneficiary Form")
         setNextButton("Next")
-        setFormLink("../BeneficiaryForm")
+        // setFormLink("../BeneficiaryForm")
         setShow(true)
     };
     const showChangeForm = () => {
         setModalTitle("Change of Ownership")
         setModalInfo("Here you can complete a form for Change of Ownership.")
+        setNextButton("Next")
         setShow(true)
     };
     const showWForm = () => {
         setModalTitle("W9 Form")
         setModalInfo("Here you can complete a W9 Form.")
+        setNextButton("Next")
         setShow(true)
     };
 
 
     return (
-    <div>
-         <h2>Please Select From The Following Forms </h2>
+    <div className="formBody">
+         <h2>Please Select The Form You Would Like to Complete</h2>
+         <h3>Tip: For details on each form, hover over the button.</h3>
         <div className="formbtn">
             <Link to="/BeneficiaryForm">
-            <Button onMouseEnter={showBeneficiaryForm} variant="primary" size="lg">
+            <Button onMouseEnter={showBeneficiaryForm} variant="primary" size="lg" className="BForm">
                Change of Beneficiary
             </Button>
             </Link>
-            <Button  onMouseEnter={showChangeForm}  variant="primary" size="lg">
+            <Button  onMouseEnter={showChangeForm}  variant="primary" size="lg" className="OForm">
                   Change of Ownership 
             </Button> 
             <Button  onMouseEnter={showWForm} variant="primary" size="lg" className="WForm">
