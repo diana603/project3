0abae64bf3047eba32d2232feadd9cbe7a8385b6
Lala <luangu2020@gmail.com>
2020-09-02 21:16:15 -0400

    changes on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,8 +21,10 @@ function Home(props) {
   // };
 
   return (
-    <Jumbotron className="LoginJumbotron" fluid >
-      <h1 className="insurance"> Welcome to East Coast Life Insurance </h1>
+    <Container className="LoginJumbotron" fluid >
+      <Jumbotron fluid>cd
+        <h1 className="insurance"> Welcome to East Coast Life Insurance </h1>
+      </Jumbotron>
       <Container className="signup">
         <Row>
           <Col className="intro" md={{ span: 8, offset: 2 }}>
@@ -45,7 +47,7 @@ function Home(props) {
                   className="m-1"
                   onClick={e => {
                     e.preventDefault();
-                    props.history.push("/users/profile");
+                    props.history.push("/profile");
                   }}
                 >
                   Members
@@ -57,7 +59,7 @@ function Home(props) {
                     className="m-1"
                     onClick={e => {
                       e.preventDefault();
-                      props.history.push("/users/login");
+                      props.history.push("/login");
                     }}
                   >
                     Login
@@ -66,7 +68,7 @@ function Home(props) {
                     className="m-1"
                     onClick={e => {
                       e.preventDefault();
-                      props.history.push("/users/register");
+                      props.history.push("/signup");
                     }}
                   >
                     Signup
@@ -84,7 +86,7 @@ function Home(props) {
       </Container>
       <Footer />
 
-    </Jumbotron>
+    </Container>
   )
 }
 
