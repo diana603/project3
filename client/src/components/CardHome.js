1fb0020c1b6b213c3abb83c95dd369dcbf1ee928
Lala <luangu2020@gmail.com>
2020-09-07 21:08:41 -0400

    removing unneeded code

--- client/src/components/CardHome.js
+++ client/src/components/CardHome.js
@@ -11,12 +11,6 @@ function CardHome(props) {
           <Card.Body>
             <Card.Title>{props.name}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{props.Subtitle}</Card.Subtitle>
-            {/* <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-    </Card.Text> */}
-            {/* <Card.Link href="./AutoIns">{props.link}</Card.Link> */}
-            {/* <Card.Link href="#">{props.link1}</Card.Link> */}
           </Card.Body>
         </Card>
       </div>
