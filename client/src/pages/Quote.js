d556c3b2dee5ead87262590b0c97a1fa59916132
Shannon Trainor <sltrainor@outlook.com>
2020-09-08 16:53:30 -0400

    added quote page with links & parallax

--- client/src/pages/Quote.js
+++ client/src/pages/Quote.js
@@ -1,17 +1,52 @@
 import React from "react";
+import {BrowserRouter as Router, Link, Route, Switch,} from 'react-router-dom';
 import Nav from "../components/Nav";
-import { Image, Col, Row, Container, Card } from "react-bootstrap";
+import { Image, Button } from "react-bootstrap";
+import { Parallax, Background } from 'react-parallax';
+
 
 
 //ABOUT EAST COAT INS/PROJECT GROUP WILL GO HERE
-function Quote () {
-        return(
-            <>
-            <Nav />
-            <h1>Quote</h1>
-            </>
-        )
-}
+const Quote = () => (
+    <div>
+        <Nav />
+        <h1>Choose Quote</h1>
+        {/* -----dynamic blur-----*/}
+        <Parallax
+            // blur={{ min: -15, max: 15 }}
+            bgImage={require('../pages/images/car-quote.jpg')}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <a href={"http://google.com"} className="autoQuote">AUTO QUOTE</a>
+            <div style={{ height: '200px' }} />
+        </Parallax>
+         {/* -----renderProp: "renderLayer"-----*/}
+         <br></br>
+         <Parallax
+            // blur={{ min: -15, max: 15 }}
+            bgImage={require('../pages/images/home-quote.jpg')}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <a href={"http://google.com"} className="homeQuote">HOME QUOTE</a>
+            <div style={{ height: '200px' }} />
+        </Parallax>
+         {/* -----renderProp: "renderLayer"-----*/}
+         <br></br>
+        <Parallax
+            // blur={{ min: -15, max: 15 }}
+            bgImage={require('../pages/images/life-quote.jpg')}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <a href={"http://google.com"} className="lifeQuote">LIFE QUOTE</a>
+            <div style={{ height: '200px' }} />
+        </Parallax>
+         {/* -----renderProp: "renderLayer"-----*/}
 
+
+    </div>
+);
 
 export default Quote;
