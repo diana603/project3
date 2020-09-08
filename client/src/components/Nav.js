34d74e676afe5099cb7dda0b0a2a3d80d8fc0a71
Shannon Trainor <sltrainor@outlook.com>
2020-09-08 15:31:26 -0400

    link changes

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,20 +10,20 @@ function NavBar () {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/Customer">Customer Forms</Nav.Link>
-                    <Nav.Link href="/About">About ECI</Nav.Link>
+                    <Nav.Link href="/Customer">Update Forms</Nav.Link>
+                    <Nav.Link href="/Quote">Get a Quote</Nav.Link>
                     <NavDropdown title="Insurance" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Auto</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Home</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Life</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Boat</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Motorcycle</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 <Nav>
                     <Nav.Link href="/Login">Login</Nav.Link>
-                    <Nav.Link eventKey={2} href="/Profile">
-                        Profile
+                    <Nav.Link eventKey={2} href="/About">
+                        About the Team
                     </Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
