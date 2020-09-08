daa7770a210f2f31a2e6d04c64b8f03d46144f6c
Shannon Trainor <sltrainor@outlook.com>
2020-09-08 15:31:17 -0400

    added quote page

---
+++ client/src/pages/Quote.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../components/Nav";
+import { Image, Col, Row, Container, Card } from "react-bootstrap";
+
+
+//ABOUT EAST COAT INS/PROJECT GROUP WILL GO HERE
+function Quote () {
+        return(
+            <>
+            <Nav />
+            <h1>Quote</h1>
+            </>
+        )
+}
+
+
+export default Quote;
