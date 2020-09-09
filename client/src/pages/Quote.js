aace9642b574214caa9a79017d49c4abf2f93877
Shannon Trainor <sltrainor@outlook.com>
2020-09-08 23:15:45 -0400

    added boat/motorcycle quote

--- client/src/pages/Quote.js
+++ client/src/pages/Quote.js
@@ -3,10 +3,9 @@ import {BrowserRouter as Router, Link, Route, Switch,} from 'react-router-dom';
 import Nav from "../components/Nav";
 import { Image, Button } from "react-bootstrap";
 import { Parallax, Background } from 'react-parallax';
+import Footer from "../components/Footer";
 
 
-
-//ABOUT EAST COAT INS/PROJECT GROUP WILL GO HERE
 const Quote = () => (
     <div>
         <Nav />
@@ -15,35 +14,64 @@ const Quote = () => (
         <Parallax
             // blur={{ min: -15, max: 15 }}
             bgImage={require('../pages/images/car-quote.jpg')}
-            bgImageAlt="the dog"
+            bgImageAlt="auto"
             strength={-200}
         >
             <a href={"http://google.com"} className="autoQuote">AUTO QUOTE</a>
             <div style={{ height: '200px' }} />
         </Parallax>
          {/* -----renderProp: "renderLayer"-----*/}
+        
          <br></br>
+        
          <Parallax
-            // blur={{ min: -15, max: 15 }}
+        // blur={{ min: -15, max: 15 }}
             bgImage={require('../pages/images/home-quote.jpg')}
-            bgImageAlt="the dog"
+            bgImageAlt="home"
             strength={-200}
         >
             <a href={"http://google.com"} className="homeQuote">HOME QUOTE</a>
             <div style={{ height: '200px' }} />
         </Parallax>
-         {/* -----renderProp: "renderLayer"-----*/}
-         <br></br>
+
+        <br></br>
+
         <Parallax
             // blur={{ min: -15, max: 15 }}
             bgImage={require('../pages/images/life-quote.jpg')}
-            bgImageAlt="the dog"
+            bgImageAlt="life"
             strength={-200}
         >
             <a href={"http://google.com"} className="lifeQuote">LIFE QUOTE</a>
             <div style={{ height: '200px' }} />
         </Parallax>
-         {/* -----renderProp: "renderLayer"-----*/}
+
+        <br></br>
+
+        <Parallax
+            // blur={{ min: -15, max: 15 }}
+            bgImage={require('../pages/images/boat-quote.jpg')}
+            bgImageAlt="boat"
+            strength={-200}
+        >
+            <a href={"http://google.com"} className="boatQuote">BOAT QUOTE</a>
+            <div style={{ height: '200px' }} />
+        </Parallax>
+        
+        <br></br>
+
+        <Parallax
+            // blur={{ min: -15, max: 15 }}
+            bgImage={require('../pages/images/motorcycle.jpg')}
+            bgImageAlt="cycle"
+            strength={-200}
+        >
+            <a href={"http://google.com"} className="cycleQuote">MOTORCYCLE QUOTE</a>
+            <div style={{ height: '200px' }} />
+        </Parallax>
+
+
+        <Footer />
 
 
     </div>
