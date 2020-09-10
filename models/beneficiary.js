9531c3226d8052b3db2912587957303075e72c5f
diana603 <stingray2091@gmail.com>
2020-09-09 20:33:53 -0400

    pushing before pulling

--- models/beneficiary.js
+++ models/beneficiary.js
@@ -29,7 +29,55 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false,
             unique: false,
-        }
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: false,
+        },
+        primaryBeneficiaryFirstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        primaryBeneficiaryLastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        percentageOfProceeds: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            unique: false,
+        },
+        primaryBeneficiaryFirstNameT: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+            unique: false,
+        }, 
+        primaryBeneficiaryLastNameT: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+            unique: false, 
+        }, 
+        percentageOfProceedsT: {
+            type: DataTypes.INTEGER, 
+            allowNull: false, 
+            unique: false,
+        }, 
+        addressT: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+            unique: false, 
+        }, 
+        
+
+
 
 
     })
