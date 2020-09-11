aff48817ec78992b444efc0c7b4bce25edccd969
Shannon Trainor <sltrainor@outlook.com>
2020-09-11 19:16:43 -0400

    compiled bootstrap, added classnames

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import "../App.css";
 import Register from "../components/SignupForm";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col } from "react-bootstrap/";
+import Nav from "../components/Nav";
 
 function Signup(props) {
   return (
+    <>
     <Container className="signup">
+      <Nav />
       <Row>
         <Col md={{ span: 8, offset: 2 }}>
           <Register {...props} />
         </Col>
       </Row>
     </Container>
+    </>
   );
 }
 
