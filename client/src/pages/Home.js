84b737cbf550dd0acb68e531c3aa7741d5ebb689
Lala <luangu2020@gmail.com>
2020-09-11 23:43:25 -0400

    added favicon, boat and moto pages, general styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from "../AuthContext";
 import "../App.css";
 import { Container, Row, Button, Col, Jumbotron } from "react-bootstrap";
-import Axios from "axios";
+// import Axios from "axios";
 import Carousel from "../components/BootstrapCarousel";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
@@ -19,7 +19,7 @@ function Home(props) {
       <Nav />
       <Container className="LoginJumbotron" fluid >
         <Jumbotron fluid className="insurancejumbo" style={{ padding: 0 }}>
-          <h1 className="insurance"> Welcome to East Coast Life Insurance </h1>
+          <h1 className="insurance"> Welcome to East Coast Insurance </h1>
         </Jumbotron>
         <Container>
           <Carousel />
