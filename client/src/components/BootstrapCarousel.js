59c42896a35a0f3c815332d169048d876e72f415
sehouse <sehouse@umich.edu>
2020-09-13 12:49:14 -0400

    BootstrapCarousel.js code cleaned.

--- client/src/components/BootstrapCarousel.js
+++ client/src/components/BootstrapCarousel.js
@@ -11,8 +11,8 @@ export class BootstrapCarousel extends Component {
               < img
                 className="d-block w-100"
                 src={'../assets/img/family_1920.jpg'}
-                alt={"image of a family"} />
-
+                alt={"image of a family"}
+              />
               < Carousel.Caption style={{ "text-color": 'black' }}>
                 < h3 > For all the little moments in life, Life Insurance is the key to protect them </h3 >
               </Carousel.Caption >
@@ -22,10 +22,8 @@ export class BootstrapCarousel extends Component {
                 style={{ height: "600px" }}
                 className="d-block w-100"
                 src={'../assets/img/fiat.jpg'}
-
-                alt={"old fiat in an autumn day"} />
-
-
+                alt={"old fiat in an autumn day"}
+              />
               < Carousel.Caption >
                 < h3 >On the road or at home, safeguard your future</h3 >
               </Carousel.Caption >
@@ -35,9 +33,8 @@ export class BootstrapCarousel extends Component {
                 style={{ 'height': "600px" }}
                 className="d-block w-100"
                 src={'../assets/img/home.jpg'}
-
-                alt={"house covered by ivy"} />
-
+                alt={"house covered by ivy"}
+              />
               < Carousel.Caption >
                 < h3 > The biggest investment in your life deserves only the best.</h3 >
               </Carousel.Caption >
@@ -47,9 +44,8 @@ export class BootstrapCarousel extends Component {
                 style={{ 'height': "600px" }}
                 className="d-block w-100"
                 src={'../assets/img/panorama_1920.jpg'}
-
-                alt={"family on top of a mountain"} />
-
+                alt={"family on top of a mountain"}
+              />
               < Carousel.Caption >
                 < h3 > From the heights to the lows, be reassured that East Coast is with you</h3 >
               </Carousel.Caption >
@@ -59,10 +55,8 @@ export class BootstrapCarousel extends Component {
                 style={{ 'height': "600px" }}
                 className="d-block w-100"
                 src={'../assets/img/pair_1920.jpg'}
-
-                alt={"couple in the woods"} />
-
-
+                alt={"couple in the woods"}
+              />
               < Carousel.Caption >
                 < h3 > Love, live, laugh with East Coast Life Insurance</h3 >
               </Carousel.Caption >
@@ -71,12 +65,9 @@ export class BootstrapCarousel extends Component {
               < img
                 style={{ 'height': "600px" }}
                 className="d-block w-100"
-
-
                 src={'../assets/img/sunset_1920.jpg'}
-                alt={"kids jumping on the sunset at the beach"} />
-
-
+                alt={"kids jumping on the sunset at the beach"}
+              />
               < Carousel.Caption >
                 < h3 > On the sunset of your life think about leaving a better future for them</h3 >
               </Carousel.Caption >
