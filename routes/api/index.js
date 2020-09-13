9139cc83c64c6fb6190da979b26c3c4c6e6acf10
sehouse <sehouse@umich.edu>
2020-09-13 11:57:05 -0400

    index.js code cleaned.

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,7 +6,5 @@ router.get("/secrets", isAuthenticated, (req, res) => {
   );
 });
 
-// router.post(endpoint or user will be using to contact back end. after first input second one will be handler.)  
-
 module.exports = router;
 
