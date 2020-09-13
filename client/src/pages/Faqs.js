d77560c03173ebe2d0e2fe1dad4edfbd96b93d41
sehouse <sehouse@umich.edu>
2020-09-13 12:25:59 -0400

    Faqs.js code cleaned.

--- client/src/pages/Faqs.js
+++ client/src/pages/Faqs.js
@@ -20,9 +20,7 @@ function Faqs() {
             <p>An entity which provides insurance is known as an insurer, insurance company, insurance carrier or underwriter. A person or entity who buys insurance is known as an insured or as a policyholder. The insurance transaction involves the insured assuming a guaranteed and known relatively small loss in the form of payment to the insurer in exchange for the insurer's promise to compensate the insured in the event of a covered loss. The loss may or may not be financial, but it must be reducible to financial terms, and usually involves something in which the insured has an insurable interest established by ownership, possession, or pre-existing relationship.</p>
 
             <p>The insured receives a contract, called the insurance policy, which details the conditions and circumstances under which the insurer will compensate the insured. The amount of money charged by the insurer to the policyholder for the coverage set forth in the insurance policy is called the premium. If the insured experiences a loss which is potentially covered by the insurance policy, the insured submits a claim to the insurer for processing by a claims adjuster. The insurer may hedge its own risk by taking out reinsurance, whereby another insurance company agrees to carry some of the risks, especially if the primary insurer deems the risk too large for it to carry.</p>
-
             <p style={{ textAlign: "right" }} ><cite>Definition by Wikipedia</cite></p>
-
           </Col>
           <Col md={{ span: 7 }}>
             < img
